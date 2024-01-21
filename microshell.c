ecb4ec732c6a7b566561cdecacdc04f2ce3933d5
Saad Karkal <Karkalsaad@gmail.com>
2024-01-22 00:49:18 +0100

    Done

--- microshell.c
+++ microshell.c
@@ -4,7 +4,7 @@
 
 int tmp;
 
-int ft_error(char *str)
+int putstr(char *str)
 {
     int i = 0;
     while(str[i])
@@ -15,9 +15,9 @@ int ft_error(char *str)
 int ft_cd(char **av, int i)
 {
     if (i != 2)
-        return (ft_error("error: cd: bad arguments\n"));
+        return (putstr("error: cd: bad arguments\n"));
     if (chdir(av[1]))
-        return (ft_error("error: cd: cannot change directory to ") && ft_error(av[1]) && ft_error("\n"));
+        return (putstr("error: cd: cannot change directory to ") && putstr(av[1]) && putstr("\n"));
     return (1);
 }
 
@@ -30,22 +30,22 @@ int ft_execute(char **av, char **env, int i)
     if (av[i] && !strcmp(av[i], "|"))
         pip = 1;
     if (pip && pipe(fd) == -1)
-        return (ft_error("error: fatal\n"));
+        return (putstr("error: fatal\n"));
     pid = fork();
     if (!pid)
     {
         av[i] = 0;
         if (dup2(tmp, 0) == -1 || close(tmp) == -1)
-            return (ft_error("error: fatal\n"));
+            return (putstr("error: fatal\n"));
         if (pip && (dup2(fd[1], 1) == -1 || close(fd[1]) == -1 || close(fd[0]) == -1))
-            return (ft_error("error: fatal\n"));
+            return (putstr("error: fatal\n"));
         execve(av[0], av, env);
-            return (ft_error("error: cannot execute ") && ft_error(av[0]) && ft_error("\n"));
+            return (putstr("error: cannot execute ") && putstr(av[0]) && putstr("\n"));
     }
     if (pip && (dup2(fd[0], tmp) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-        return (ft_error("error: fatal\n"));
+        return (putstr("error: fatal\n"));
     if (!pip && (dup2(0, tmp) == -1 || waitpid(pid, NULL, 0) == -1))
-        return (ft_error("error: fatal\n"));
+        return (putstr("error: fatal\n"));
     return 0;
 }
 
@@ -60,7 +60,7 @@ int main(int ac, char **av, char **env)
         av += i;
         i = 0;
         while(av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
-            i ++;
+            i++;
         if (!strcmp(av[0], "cd"))
             ft_cd(av, i);
         else if (i)
